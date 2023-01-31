66ca003fa56926ef58943377134d7764717b9aca
Miguel Sama <miguelsamamerino@gmail.com>
2023-01-31 14:53:08 +0100

    add parseCartridgeHeader
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cart/cart.go
+++ pkg/cart/cart.go
@@ -15,22 +15,34 @@ type CartridgeHeader struct {
 	// ManufacturerCode in older cartridges was part of the title. The purpose of this si unknown.
 	ManufacturerCode [4]byte
 	// CgbFlag in older cartridges was part of the title. It decides whether enable color mode.
-	CgbFlag              byte
-	NewLicenseeCode      [2]byte
-	SgbFlag              byte
-	CartridgeType        byte
-	RomSize              byte
-	RamSize              byte
-	DestinationCode      byte
-	OldLicenseeCode      byte
+	CgbFlag byte
+	// NewLicenseeCode contains a two-character ASCII “licensee code” indicating the game’s publisher.
+	NewLicenseeCode [2]byte
+	// SgbFlag specifies whether the game supports SGB functions.
+	SgbFlag byte
+	// CartridgeType indicates what kind of hardware is present on the cartridge.
+	CartridgeType byte
+	// RomSize indicates how much ROM is present on the cartridge.
+	RomSize byte
+	// RamSize indicates how much RAM is present on the cartridge, if any.
+	RamSize byte
+	// DestinationCode specifies whether this version of the game is intended to be sold in Japan or elsewhere.
+	DestinationCode byte
+	// OldLicenseeCode is used in older (pre-SGB) cartridges to specify the game’s publisher.
+	OldLicenseeCode byte
+	// MaskRomVersionNumber specifies the version number of the game. It is usually 0x0.
 	MaskRomVersionNumber byte
-	HeaderCheckSum       byte
-	GlobalChecksum       [2]byte
+	// HeaderCheckSum contains an 8-bit checksum computed from the cartridge header bytes 0x134–0x14C.
+	HeaderCheckSum byte
+	// GlobalChecksum contain a 16-bit (big-endian) checksum simply computed as the sum of all the bytes of
+	// the cartridge ROM (except these two checksum bytes).
+	GlobalChecksum [2]byte
 }
 
 // Cartridge implements all the logic regarding GB cartridges
 type Cartridge struct {
-	rawData []byte
+	CartridgeHeader *CartridgeHeader
+	rawData         []byte
 }
 
 // NewCartridge returns a pointer to Cartridge given a Rom path
@@ -40,9 +52,28 @@ func NewCartridge(romPath string) (*Cartridge, error) {
 		return nil, fmt.Errorf("error while loading ROM cartridges - %v", err)
 	}
 
-	// Perform checks that the ROM is ok
-
 	return &Cartridge{
-		rawData: romData,
+		CartridgeHeader: parseCartridgeHeader(romData),
+		rawData:         romData,
 	}, nil
+}
+
+func parseCartridgeHeader(cartridgeRawData []byte) *CartridgeHeader {
+	cartridgeHeader := &CartridgeHeader{}
+	copy(cartridgeHeader.EntryPoint[:], cartridgeRawData[EntryPointAddrStart:EntryPointAddrEnd+1])
+	copy(cartridgeHeader.NintendoLogo[:], cartridgeRawData[NintendoLogoAddrStart:NintendoLogoAddrEnd+1])
+	copy(cartridgeHeader.Title[:], cartridgeRawData[TitleAddrStart:TitleAddrEnd+1])
+	copy(cartridgeHeader.ManufacturerCode[:], cartridgeRawData[ManufacturerAddrStart:ManufacturerAddrEnd+1])
+	cartridgeHeader.CgbFlag = cartridgeRawData[CgbFlagAddr]
+	copy(cartridgeHeader.NewLicenseeCode[:], cartridgeRawData[NewLicenseeCodeAddrStart:NewLicenseeCodeAddrEnd+1])
+	cartridgeHeader.SgbFlag = cartridgeRawData[SgbFlagAddr]
+	cartridgeHeader.CartridgeType = cartridgeRawData[CartridgeTypeAddr]
+	cartridgeHeader.RomSize = cartridgeRawData[RomSizeAddr]
+	cartridgeHeader.RamSize = cartridgeRawData[RamSizeAddr]
+	cartridgeHeader.DestinationCode = cartridgeRawData[DestinationCodeAddr]
+	cartridgeHeader.OldLicenseeCode = cartridgeRawData[OldLicenseeCodeAddr]
+	cartridgeHeader.MaskRomVersionNumber = cartridgeRawData[MaskRomVersionNumberAddr]
+	cartridgeHeader.HeaderCheckSum = cartridgeRawData[HeaderChecksumAddr]
+	copy(cartridgeHeader.GlobalChecksum[:], cartridgeRawData[GlobalChecksumAddrStart:GlobalChecksumAddrEnd+1])
+	return cartridgeHeader
 }
