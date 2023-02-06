f6aa059c330492682de32ef561bac9b484ff7005
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-06 13:47:48 +0100

    add mnemonic to instruction map
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cpu/instruction.go
+++ pkg/cpu/instruction.go
@@ -13,12 +13,14 @@ type Instruction struct {
 	Condition int
 	// Parameter is specially used for CB.
 	Parameter byte
+	// Mnemonic is the human-readable instruction.
+	Mnemonic string
 }
 
 var instructionsMap = map[byte]Instruction{
-	0x00: {Type: inNop, AddressingMode: amImp},                     // 0x00 NOP
-	0x05: {Type: inDec, AddressingMode: amR, RegisterType1: rtB},   // 0x05 DEC B
-	0x0E: {Type: inLd, AddressingMode: amRnD8, RegisterType1: rtC}, // 0x0E LD C, d8
-	0xAF: {Type: inXor, AddressingMode: amR, RegisterType1: rtA},   // 0xAF XOR A
-	0xC3: {Type: inJp, AddressingMode: amD16},                      // 0xC3 JP a16
+	0x00: {Type: inNop, Mnemonic: "NOP"},                                                 // 0x00 NOP
+	0x05: {Type: inDec, AddressingMode: amR, RegisterType1: rtB, Mnemonic: "DEC B"},      // 0x05 DEC B
+	0x0E: {Type: inLd, AddressingMode: amRnD8, RegisterType1: rtC, Mnemonic: "LD C, d8"}, // 0x0E LD C, d8
+	0xAF: {Type: inXor, AddressingMode: amR, RegisterType1: rtA, Mnemonic: "XOR A"},      // 0xAF XOR A
+	0xC3: {Type: inJp, AddressingMode: amD16, Mnemonic: "JP a16"},                        // 0xC3 JP a16
 }
