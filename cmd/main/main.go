99fc09e9c8d0a0ab4001102785a1769338e9f6be
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-06 21:54:38 +0100

    add sample main
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"flag"
+	"github.com/mikeletux/goboy/pkg/bus"
+	"github.com/mikeletux/goboy/pkg/cart"
+	"github.com/mikeletux/goboy/pkg/cpu"
+	"github.com/mikeletux/goboy/pkg/log"
+)
+
+func main() {
+	romCartridgePath := flag.String("romPath", "", "Path to the Gameboy rom to load")
+	flag.Parse()
+
+	// Build stdout logger
+	logger := log.NewBuiltinStdoutLogger()
+
+	// Build cartridge
+	cartridge, err := cart.NewCartridge(*romCartridgePath, logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	cartridge.LogCartridgeHeaderInfo()
+
+	// Build memory bus
+	memoryBus := bus.NewBus(cartridge, logger)
+
+	// Build CPU
+	gbCpu := cpu.Init(memoryBus, logger)
+
+	for {
+		gbCpu.Step()
+	}
+}
