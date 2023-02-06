2ba6f3fedba6b0fd399ddba029b922bbb4fca04d
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-07 00:06:59 +0100

    delete instruction map and add more exec functions
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cpu/cpu_proc.go
+++ pkg/cpu/cpu_proc.go
@@ -1,57 +1,44 @@
 package cpu
 
-var execInstructionMap = map[int]func(c *CPU){
-	inNone: nil,
-	inNop:  nil,
-	inLd:   nil,
-	inInc:  nil,
-	inDec:  nil,
-	inRlca: nil,
-	inAdd:  nil,
-	inRrca: nil,
-	inStop: nil,
-	inRla:  nil,
-	inJr:   nil,
-	inRra:  nil,
-	inDaa:  nil,
-	inCpl:  nil,
-	inScf:  nil,
-	inCcf:  nil,
-	inHalt: nil,
-	inAdc:  nil,
-	inSub:  nil,
-	inSbc:  nil,
-	inAnd:  nil,
-	inXor:  xorExecFunc,
-	inOr:   nil,
-	inCp:   nil,
-	inPop:  nil,
-	inJp:   nil,
-	inPush: nil,
-	inRet:  nil,
-	inCb:   nil,
-	inCall: nil,
-	inReti: nil,
-	inLdh:  nil,
-	inJphl: nil,
-	inDi:   nil,
-	inEi:   nil,
-	inRst:  nil,
-	inErr:  nil,
-	// CB instructions
-	inRlc:  nil,
-	inRrc:  nil,
-	inRl:   nil,
-	inRr:   nil,
-	inSla:  nil,
-	inSra:  nil,
-	inSwap: nil,
-	inSrl:  nil,
-	inBit:  nil,
-	inRes:  nil,
-	inSet:  nil,
+func nopExecFunc(c *CPU) {
+	c.emulateCpuCycles(4)
+	return
 }
 
 func xorExecFunc(c *CPU) {
+	c.registers.A ^= c.registers.A // This is for sure wrong. Double check
+	if c.registers.A == 0x0 {
+		c.registers.SetFZ(true)
+	}
+	c.emulateCpuCycles(4)
+}
 
+func jpExecFunc(c *CPU) {
+	if checkCondition(c) {
+		c.registers.PC = c.FetchedData
+		c.emulateCpuCycles(1)
+	}
+}
+
+func checkCondition(c *CPU) bool {
+	fz := c.registers.GetFZ()
+	fc := c.registers.GetFC()
+
+	switch c.CurrentInstruction.Condition {
+	case ctNone:
+		return true
+	case ctZ:
+		return fz
+	case ctNZ:
+		return !fz
+	case ctC:
+		return fc
+	case ctNC:
+		return !fc
+	}
+	return true // This never should be reached
+}
+
+func diExecFunc(c *CPU) {
+	c.EnableMasterInterruptions = false
 }
