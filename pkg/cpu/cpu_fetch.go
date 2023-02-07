fbff2df722843133770c2fae099ca000be985991
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-07 12:29:21 +0100

    add db docs
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cpu/cpu_fetch.go
+++ pkg/cpu/cpu_fetch.go
@@ -10,6 +10,18 @@ func (c *CPU) fetchData() error {
 	case amImp:
 		return nil
 
+	case amRnD16:
+		// TBD
+		return nil
+
+	case amRnR:
+		// TBD
+		return nil
+
+	case amMRnR:
+		// TBD
+		return nil
+
 	case amR:
 		fetchedData, err := c.registers.FetchDataFromRegisters(c.CurrentInstruction.RegisterType1)
 		if err != nil {
@@ -23,6 +35,38 @@ func (c *CPU) fetchData() error {
 		c.emulateCpuCycles(1)
 		return nil
 
+	case amRnMR:
+		// TBD
+		return nil
+
+	case amRnHLI:
+		// TBD
+		return nil
+
+	case amRnHLD:
+		// TBD
+		return nil
+
+	case amHLInR:
+		// TBD
+		return nil
+
+	case amHLRnR:
+		// TBD
+		return nil
+
+	case amRnA8:
+		// TBD
+		return nil
+
+	case amA8nR:
+		// TBD
+		return nil
+
+	case amHLnSPR:
+		// TBD
+		return nil
+
 	case amD16:
 		var low = c.bus.BusRead(c.registers.GetPCAndIncrement())
 		c.emulateCpuCycles(1)
@@ -33,6 +77,30 @@ func (c *CPU) fetchData() error {
 		c.FetchedData = uint16(low) | uint16(high)<<8
 		return nil
 
+	case amD8:
+		// TBD
+		return nil
+
+	case amD16nR:
+		// TBD
+		return nil
+
+	case amMRnD8:
+		// TBD
+		return nil
+
+	case amMR:
+		// TBD
+		return nil
+
+	case amA16nR:
+		// TBD
+		return nil
+
+	case amRnA16:
+		// TBD
+		return nil
+		
 	// To be done still
 	default:
 		return fmt.Errorf("addressing mode %d doesn't exist", c.CurrentInstruction.AddressingMode)
