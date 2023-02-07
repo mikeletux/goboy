0dc1a83f2913bf97ef8136ae0d01b80dd9e581f8
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-07 23:40:09 +0100

    add BusRead16
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/bus/bus.go
+++ pkg/bus/bus.go
@@ -8,6 +8,7 @@ import (
 type DataBusInterface interface {
 	BusRead(address uint16) byte
 	BusWrite(address uint16, value byte)
+	BusRead16(address uint16) uint16
 	BusWrite16(address uint16, value uint16)
 }
 
@@ -18,7 +19,7 @@ type Bus struct {
 	logger    log.Logger
 }
 
-// NewBus initializes a bus given a type that implements the cart.CartridgeInterface interface
+// NewBus initializes a bus given a type that implements the cart.CartridgeInterface interface.
 func NewBus(cartridge cart.CartridgeInterface, logger log.Logger) *Bus {
 	return &Bus{
 		Cartridge: cartridge,
@@ -26,7 +27,7 @@ func NewBus(cartridge cart.CartridgeInterface, logger log.Logger) *Bus {
 	}
 }
 
-// BusRead given an address it returns the value from that bus memory area
+// BusRead given an address it returns the value from that bus memory area.
 func (b *Bus) BusRead(address uint16) byte {
 	if address <= RomBank01NNEnd {
 		return b.Cartridge.CartRead(address)
@@ -49,5 +50,15 @@ func (b *Bus) BusWrite(address uint16, value byte) {
 	b.logger.Fatalf("bus address 0x%X yet not implemented to write", address)
 }
 
-// BusWrite16 writes two bytes into bus given a bus memory area
-func (b *Bus) BusWrite16(address uint16, value uint16) {}
+// BusRead16 given an address it returns the 16 bit value from that area.
+func (b *Bus) BusRead16(address uint16) uint16 {
+	low := b.BusRead(address)
+	high := b.BusRead(address + 1)
+	return uint16(high<<8 | low)
+}
+
+// BusWrite16 writes 16 bit value into bus given a bus memory area.
+func (b *Bus) BusWrite16(address uint16, value uint16) {
+	b.BusWrite(address, byte(value&0xFF))        // Low
+	b.BusWrite(address+1, byte((value>>8)&0xFF)) // High
+}
