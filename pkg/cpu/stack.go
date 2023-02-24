8c5ed486a14f081d45d3fccb375ee0273d50135a
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-24 16:49:28 +0100

    add stack functions
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

---
+++ pkg/cpu/stack.go
@@ -0,0 +1,23 @@
+package cpu
+
+func (c *CPU) stackPush(value byte) {
+	c.registers.SP--
+	c.bus.BusWrite(c.registers.SP, value)
+	c.emulateCpuCycles(1)
+}
+func (c *CPU) stackPush16(value uint16) {
+	c.stackPush(byte(value >> 8 & 0xFF))
+	c.stackPush(byte(value & 0xFF))
+}
+
+func (c *CPU) stackPop() byte {
+	value := c.bus.BusRead(c.registers.SP)
+	c.registers.SP++
+	c.emulateCpuCycles(1)
+	return value
+}
+func (c *CPU) stackPop16() uint16 {
+	low := uint16(c.stackPop())
+	high := uint16(c.stackPop())
+	return high<<8 | low
+}
