c63d3814cdf329055c629158d252f67b6c07b458
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-28 21:53:58 +0100

    modify mock for bus
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cpu/mock.go
+++ pkg/cpu/mock.go
@@ -1,27 +1,36 @@
 package cpu
 
-// BusMock is used to test CPU
-type BusMock struct {
-	Data []byte
+// BusMapMock is used to test proc funcs
+type BusMapMock struct {
+	Data map[uint16]byte
 }
 
-func (b *BusMock) BusRead(address uint16) byte {
+// NewBusMapMock returns an empty BusMapMock ready to use
+func NewBusMapMock() *BusMapMock {
+	return &BusMapMock{
+		Data: make(map[uint16]byte),
+	}
+}
+
+func (b *BusMapMock) BusRead(address uint16) byte {
 	return b.Data[address]
 }
 
-func (b *BusMock) BusRead16(address uint16) uint16 {
-	return 0xFFFF // TBD!!!
+func (b *BusMapMock) BusRead16(address uint16) uint16 {
+	low := b.Data[address]
+	high := b.Data[address + 1]
+	return uint16(high<<8 | low)
 }
 
-func (b *BusMock) BusWrite(address uint16, value byte) {
-	if int(address) > len(b.Data) {
-		return
-	}
-
+func (b *BusMapMock) BusWrite(address uint16, value byte) {
 	b.Data[address] = value
 	return
 }
 
-func (b *BusMock) BusWrite16(address uint16, value uint16) {
-	return // TBD!!!!
+func (b *BusMapMock) BusWrite16(address uint16, value uint16) {
+	low := byte(value & 0xFF)
+	high := byte(value>>8 & 0xFF)
+	b.Data[address] = low
+	b.Data[address+1] = high
+	return
 }
