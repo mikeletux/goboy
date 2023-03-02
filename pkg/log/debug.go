e54ba5d577ea4450b583cd207f49e74b3cad13a8
Miguel Sama <miguelsamamerino@gmail.com>
2023-03-02 12:37:08 +0100

    add log to file
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/log/debug.go
+++ pkg/log/debug.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"io"
 	"log"
 	"os"
 )
@@ -11,29 +12,84 @@ type Logger interface {
 	Debugf(format string, args ...any)
 	Fatal(args ...any)
 	Fatalf(format string, args ...any)
+	Close()
 }
 
 // BuiltinLogger is a simple wrapper to log.Logger.
 type BuiltinLogger struct {
-	logger *log.Logger
+	noLog   bool
+	logFile *os.File
+	logger  *log.Logger
 }
 
 // NewBuiltinStdoutLogger returns a simple BuiltinLogger that only logs on stdout.
-func NewBuiltinStdoutLogger() *BuiltinLogger {
-	return &BuiltinLogger{
-		logger: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile),
+func NewBuiltinStdoutLogger(logStdout bool, logPath string) (*BuiltinLogger, error) {
+	var noLog bool
+	var logFile *os.File
+	var writer io.Writer
+
+	switch {
+	case !logStdout && len(logPath) == 0:
+		noLog = true
+		writer = os.Stdout
+		break
+
+	case logStdout && len(logPath) > 0:
+		logFile, err := openFile(logPath)
+		if err != nil {
+			return nil, err
+		}
+		writer = io.MultiWriter(os.Stdout, logFile)
+		break
+
+	case logStdout:
+		writer = os.Stdout
+		break
+
+	case len(logPath) > 0:
+		logFile, err := openFile(logPath)
+		if err != nil {
+			return nil, err
+		}
+		writer = logFile
+		break
 	}
+
+	return &BuiltinLogger{
+		noLog:   noLog,
+		logFile: logFile,
+		logger:  log.New(writer, "", log.Ldate|log.Ltime|log.Lshortfile),
+	}, nil
 }
 
 func (b *BuiltinLogger) Debug(args ...any) {
-	b.logger.Println(args...)
+	if !b.noLog {
+		b.logger.Println(args...)
+	}
 }
 func (b *BuiltinLogger) Debugf(format string, args ...any) {
-	b.logger.Printf(format, args...)
+	if !b.noLog {
+		b.logger.Printf(format, args...)
+	}
 }
 func (b *BuiltinLogger) Fatal(args ...any) {
 	b.logger.Fatalln(args...)
 }
 func (b *BuiltinLogger) Fatalf(format string, args ...any) {
 	b.logger.Fatalf(format, args...)
+}
+
+func (b *BuiltinLogger) Close() {
+	if b.logFile != nil {
+		b.logFile.Close()
+	}
+}
+
+func openFile(logFile string) (*os.File, error) {
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
