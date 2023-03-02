d3ac9a837bb834addeb4894710d58fe436959403
Miguel Sama <miguelsamamerino@gmail.com>
2023-03-02 12:37:28 +0100

    update mocklog
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/log/mock.go
+++ pkg/log/mock.go
@@ -1,8 +1,10 @@
 package log
+
 // NilLogger is a dummy struct that allows to avoid logging anything
 type NilLogger struct{}
 
-func (n *NilLogger)Debug(args ...any){ return }
-func (n *NilLogger)Debugf(format string, args ...any){ return }
-func (n *NilLogger)Fatal(args ...any){ return }
-func (n *NilLogger)Fatalf(format string, args ...any){ return }
+func (n *NilLogger) Debug(args ...any)                 { return }
+func (n *NilLogger) Debugf(format string, args ...any) { return }
+func (n *NilLogger) Fatal(args ...any)                 { return }
+func (n *NilLogger) Fatalf(format string, args ...any) { return }
+func (n *NilLogger) Close()                            {}
