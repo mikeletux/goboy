d18ad6fd87da083fd7a13fc6211dacd6db7b9fde
Miguel Sama <miguelsamamerino@gmail.com>
2023-03-07 17:20:52 +0100

    extended bus for more io
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/bus/io.go
+++ pkg/bus/io.go
@@ -3,8 +3,8 @@ package bus
 import "github.com/mikeletux/goboy/pkg/log"
 
 type io struct {
-	logger     log.Logger
-	serialData [2]byte
+	logger      log.Logger
+	ioRegisters [IORegistersEnd - IORegistersStart + 1]byte
 }
 
 func NewIO(logger log.Logger) *io {
@@ -14,30 +14,13 @@ func NewIO(logger log.Logger) *io {
 }
 
 func (i *io) IORead(address uint16) byte {
-	if address == 0xFF01 {
-		return i.serialData[0]
-	}
-
-	if address == 0xFF02 {
-		return i.serialData[1]
-	}
-
 	if address == 0xFF44 {
 		return 0x90 // Hardcoded value for Gameboy doctor
 	}
 
-	// i.logger.Debug("unsupported io read")
-	return 0
+	return i.ioRegisters[address-IORegistersStart]
 }
 
 func (i *io) IOWrite(address uint16, data byte) {
-	if address == 0xFF01 {
-		i.serialData[0] = data
-	}
-
-	if address == 0xFF02 {
-		i.serialData[1] = data
-	}
-
-	// i.logger.Debug("unsupported io write")
+	i.ioRegisters[address-IORegistersStart] = data
 }
