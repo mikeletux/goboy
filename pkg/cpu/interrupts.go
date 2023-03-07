3a61b47c73b1047cab40bf0160271a54355c28d1
Miguel Sama <miguelsamamerino@gmail.com>
2023-03-07 17:22:01 +0100

    add interrupts
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/cpu/interrupts.go
+++ pkg/cpu/interrupts.go
@@ -1,8 +1,48 @@
 package cpu
 
-func (c *CPU) handleInterrupt(address uint16) {
+const (
+	vblankInterruptFlag  byte = 0x1
+	lcdStatInterruptFlag byte = 0x2
+	timerInterruptFlag   byte = 0x4
+	serialInterruptFlag  byte = 0x8
+	joypadInterruptFlag  byte = 0x16
+)
+
+const (
+	vblankInterruptAddr  uint16 = 0x40
+	lcdStatInterruptAddr uint16 = 0x48
+	timerInterruptAddr   uint16 = 0x50
+	serialInterruptAddr  uint16 = 0x58
+	joypadInterruptAddr  uint16 = 0x60
+)
+
+func (c *CPU) pushPCToStack(address uint16) {
 	c.stackPush16(c.registers.PC)
 	c.registers.PC = address
 }
 
+func (c *CPU) interruptCheck(address uint16, interruptType byte) bool {
+	ieRegister := c.bus.BusRead(interruptEnableAddr)
+	ifRegister := c.bus.BusRead(interruptFlagIOAddr)
 
+	if ifRegister&interruptType == interruptType &&
+		ieRegister&interruptType == interruptType {
+
+		c.pushPCToStack(address)
+		c.bus.BusWrite(address, ifRegister & ^interruptType)
+		c.Halted = false
+
+		return true
+	}
+
+	return false
+}
+
+func (c *CPU) handleInterruptions() {
+	if c.interruptCheck(vblankInterruptAddr, vblankInterruptFlag) {
+	} else if c.interruptCheck(lcdStatInterruptAddr, lcdStatInterruptFlag) {
+	} else if c.interruptCheck(timerInterruptAddr, timerInterruptFlag) {
+	} else if c.interruptCheck(serialInterruptAddr, serialInterruptFlag) {
+	} else if c.interruptCheck(joypadInterruptAddr, joypadInterruptFlag) {
+	}
+}
