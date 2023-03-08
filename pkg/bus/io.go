c42fe5ce117fa301bc3628c3c14505aa966b975b
Miguel Sama <miguelsamamerino@gmail.com>
2023-03-08 20:02:37 +0100

    add serial registers and if one
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/bus/io.go
+++ pkg/bus/io.go
@@ -2,12 +2,20 @@ package bus
 
 import "github.com/mikeletux/goboy/pkg/log"
 
+// Register addresses
 const (
+	serialTransferDataAddr    uint16 = 0xFF01
+	serialTransferControlAddr uint16 = 0xFF02
+
 	divRegisterAddr  uint16 = 0xFF04
 	timaRegisterAddr uint16 = 0xFF05
 	tmaRegisterAddr  uint16 = 0xFF06
 	tacRegisterAddr  uint16 = 0xFF07
 
+	interruptFlagRegisterAddr uint16 = 0xFF0F
+)
+
+const (
 	initialDivRegisterValue uint16 = 0xABCC
 )
 
@@ -18,16 +26,23 @@ type timer struct {
 	tacReg  byte   // FF07
 }
 
+type serial struct {
+	serialTransferData    byte // FF01
+	serialTransferControl byte // FF02
+}
+
 type io struct {
 	logger log.Logger
 	// ioRegisters [IORegistersEnd - IORegistersStart + 1]byte
-
-	timer *timer
+	serial *serial
+	timer  *timer
+	ifReg  byte // Interrupt Flag FF0F
 }
 
 func NewIO(logger log.Logger) *io {
 	io := &io{
 		logger: logger,
+		serial: &serial{},
 		timer: &timer{
 			divReg: initialDivRegisterValue,
 		},
@@ -42,6 +57,10 @@ func (i *io) IORead(address uint16) byte {
 	}
 
 	switch address { // This switch is for special cases (Like 16bit Timer DIV register)
+	case serialTransferDataAddr:
+		return i.serial.serialTransferData
+	case serialTransferControlAddr:
+		return i.serial.serialTransferControl
 	case divRegisterAddr:
 		return byte(i.timer.divReg >> 8)
 	case timaRegisterAddr:
@@ -50,6 +69,8 @@ func (i *io) IORead(address uint16) byte {
 		return i.timer.tmaReg
 	case tacRegisterAddr:
 		return i.timer.tacReg
+	case interruptFlagRegisterAddr:
+		return i.ifReg
 	default:
 		return 0x0
 	}
@@ -59,6 +80,10 @@ func (i *io) IORead(address uint16) byte {
 
 func (i *io) IOWrite(address uint16, data byte) {
 	switch address { // This switch is for special cases (Like 16bit Timer DIV register)
+	case serialTransferDataAddr:
+		i.serial.serialTransferData = data
+	case serialTransferControlAddr:
+		i.serial.serialTransferControl = data
 	case divRegisterAddr:
 		i.timer.divReg = 0
 	case timaRegisterAddr:
@@ -67,6 +92,8 @@ func (i *io) IOWrite(address uint16, data byte) {
 		i.timer.tmaReg = data
 	case tacRegisterAddr:
 		i.timer.tacReg = data
+	case interruptFlagRegisterAddr:
+		i.ifReg = data
 	}
 
 	// i.ioRegisters[address-IORegistersStart] = data
